064055aa862dbaa4f382edc52800633f77da3cec
newpoopuk <newpoopuk4@gmail.com>
2025-03-21 21:14:29 +0700

    Your commit message

--- backend/main.py
+++ backend/main.py
@@ -392,7 +392,7 @@ async def process_data(date: str = Query(..., description="Date in DD-MM-YYYY fo
 # Middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
